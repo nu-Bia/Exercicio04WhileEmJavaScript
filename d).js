9e82dd621ed5f10db156a843ec5aaba7f36f69aa
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-11-04 13:07:05 -0300

    Add files via upload

---
+++ d).js
@@ -0,0 +1,11 @@
+function soma() {
+    var contador = 0;
+
+while (contador <= 20) {
+var contador = contador + 1;
+if(contador % 2 == 1){
+document.write(contador + " - ")
+        }
+    
+    }
+}
